ec83a0bb067bcd5a16614e7a48544729d02844b3
Sacrlet339 <54624514+Sacrlet339@users.noreply.github.com>
2022-05-18 19:21:54 +0200

    added slider underline

--- js/controls.js
+++ js/controls.js
@@ -6,10 +6,6 @@ class Slider {
       this.prev = document.getElementById("prev");
       this.nbr = this.slide.length;
       this.nbrClick = 0;
-      console.log(this.nbrClick, (this.nbrClick - 1), (this.nbrClick - 2));
-      if(screen.width <= 600){
-        console.log('mobile')
-      }
     }
   
     click() {
@@ -45,7 +41,6 @@ class Slider {
             this.slide[i].classList = "slide animate__slideOutLeft hidden";
             this.nav[i].classList = "carousel__button";
           }
-          console.log(this.nbrClick, (this.nbrClick - 1), (this.nbrClick - 2));
           if((this.nbrClick) < 0){
             this.nbrClick = this.nbr;
           }
@@ -65,7 +60,6 @@ class Slider {
 
           if(screen.width >= 600){
             if((this.nbrClick - 2) < 0){
-              console.log('*',this.nbr -2);
               this.slide[this.nbr -1].classList = "slide right";
               this.nav[this.nbr-1].classList = "carousel__button carousel__button--selected";
             }else{
@@ -79,7 +73,6 @@ class Slider {
 
     }
     bottomNav(x){
-      console.log('i got called', x);
       this.nbrClick = x;
 
       for (let i = 0; i < this.nbr; i++) {
@@ -113,11 +106,30 @@ class Slider {
       }
 
     }
+    resize(){
+      for (let i = 0; i < this.nbr; i++) {
+        this.slide[i].classList.add('animate__animated', 'animate__slideOutLeft');
+        this.slide[i].classList = "slide animate__slideOutLeft hidden";
+        this.nav[i].classList = "carousel__button";
+  }
+
+      this.slide[this.nbrClick % this.nbr].classList = "slide left";
+      this.slide[(this.nbrClick + 1) % this.nbr].classList = "slide center";
+      //nav
+      this.nav[this.nbrClick % this.nbr].classList = "carousel__button carousel__button--selected";
+      this.nav[(this.nbrClick + 1) % this.nbr].classList = "carousel__button carousel__button--selected";
+
+      if(screen.width >= 600){
+          this.slide[(this.nbrClick + 2) % this.nbr].classList = "slide right";
+          this.nav[(this.nbrClick + 2) % this.nbr].classList = "carousel__button carousel__button--selected";
+      }
+
+    }
   }
 
   
   let slider = new Slider();
   window.addEventListener('resize', function(event){
-    console.log('resize');
+    slider.resize();
   });
   slider.click();
